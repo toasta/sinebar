fe6642b997c4d41c7cdc4a380c00de09e491e291
Benedikt Fraunhofer <benedikt.fraunhofer@globalways.net>
2018-01-18 22:07:27 +0100

    .gitignore

---
+++ lib/get-libs.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+wget "https://jqueryui.com/resources/download/jquery-ui-1.12.1.zip"
+unzip jquery-ui-1.12.1.zip 
+wget "https://code.jquery.com/jquery-3.2.1.min.js"
+wget "https://jqueryui.com/resources/download/jquery-ui-themes-1.12.1.zip"
+unzip jquery-ui-themes-1.12.1.zip 
